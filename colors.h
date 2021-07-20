50e0bb7ef2bd02c1665b5dc15bcf6fba14a6572d
ronejfourn <rs2102@protonmail.com>
2021-07-20 19:39:55 +0545

    user interaction boilerplate

--- colors.h
+++ colors.h
@@ -1,2 +1,16 @@
 #define BG 46, 52, 64, 255
 #define BG1 76, 86, 106, 255
+
+#define RED 191,97,106
+#define ORANGE 208,135,112
+#define YELLOW 235,203,139 
+#define GREEN 163,190,140
+#define PURPLE 180,142,173
+#define BLUE 94,129,172 
+
+#define INSERT ORANGE, 255
+#define DELETE RED, 255
+#define WIRING BLUE, 255
+#define RUNNING GREEN, 255
+#define NORMAL YELLOW, 255
+
--- main.c
+++ main.c
@@ -10,14 +10,17 @@ SDL_Renderer* renderer = NULL;
 #define W_WIDTH  500
 #define W_HEIGHT 500
 
-#define G_SIZE_DEF 30
-#define G_SIZE_MIN 10
-#define G_SIZE_MAX 40
+#define M_WIDTH  150
 
-#define G_WIDTH  (1920 / G_SIZE_MIN)
-#define G_HEIGHT (1080 / G_SIZE_MIN)
+#define G_SIZE 25
 
-typedef enum {none, insert, delete, wire, run} Modes;
+#define G_WIDTH  1000
+#define G_HEIGHT (G_WIDTH * 3 / 4)
+
+#define CP_MAX 256
+
+typedef enum {none, insert, delete, wiring, running} Modes;
+typedef enum {state, probe, and, or, not} Builtins;
 
 typedef struct{
     char x, y;
@@ -52,16 +55,59 @@ void close(){
     exit(0);
 }
 
-void DrawGrid(int size){
-    SDL_Rect square = {.w = size - 1, .h = size - 1};
+void DrawGrid(){
+    SDL_Rect square = {.w = G_SIZE - 1, .h = G_SIZE - 1};
     SDL_SetRenderDrawColor(renderer, BG1);
-    for (int i = 0; i < G_WIDTH; i ++){
-        for (int j = 0; j < G_HEIGHT; j ++){
-            square.x = i * size + 200;
-            square.y = j * size;
+    for (int i = 0; i < G_WIDTH / G_SIZE; i ++){
+        for (int j = 0; j < G_HEIGHT / G_SIZE; j ++){
+            square.x = i * G_SIZE + M_WIDTH;
+            square.y = j * G_SIZE;
             SDL_RenderFillRect(renderer, &square);
         }
     }
+}
+
+void Highlight(Pair pos, Modes mode){
+    SDL_Rect border;
+    switch (mode){
+        case insert:
+            SDL_SetRenderDrawColor(renderer, INSERT);
+            break;
+        case delete:
+            SDL_SetRenderDrawColor(renderer, DELETE);
+            break;
+        case wiring:
+            SDL_SetRenderDrawColor(renderer, WIRING);
+            break;
+        case running:
+            SDL_SetRenderDrawColor(renderer, RUNNING);
+            break;
+        default:
+            SDL_SetRenderDrawColor(renderer, NORMAL);
+            break;
+    }
+    border.x = pos.x * G_SIZE - 1 + M_WIDTH;
+    border.y = pos.y * G_SIZE - 1;
+    border.w = G_SIZE + 1;
+    border.h = G_SIZE + 1;
+    SDL_RenderDrawRect(renderer, &border);
+    border.x = pos.x * G_SIZE + M_WIDTH;
+    border.y = pos.y * G_SIZE;
+    border.w = G_SIZE - 1;
+    border.h = G_SIZE - 1;
+    SDL_RenderDrawRect(renderer, &border);
+}
+
+void CheckPlaceble(Pair start, Pair size, char * grid){
+
+}
+
+void InsertComponent(Pair pos){
+    printf("Inserted at %d, %d\n", pos.x, pos.y);
+}
+
+void DeleteComponent(Pair pos){
+    printf("Deleted at %d, %d\n", pos.x, pos.y);
 }
 
 int main(int argc, char** args){
@@ -70,15 +116,22 @@ int main(int argc, char** args){
     SDL_SetWindowResizable(window, SDL_TRUE);
     SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
 
-    int size = G_SIZE_DEF;
     Modes mode = none;
+    Builtins comp = state;
 
-    char componentCount = 0;
-    char grid[G_WIDTH * G_HEIGHT];
-    Component componentList[256];
+    unsigned char componentCount = 0;
+    char grid[G_WIDTH / G_SIZE * G_HEIGHT / G_SIZE];
+    Component componentList[CP_MAX];
+    Pair mousepos;
+    int mousex, mousey;
 
     SDL_Event e;
     while(1){
+
+        SDL_GetMouseState(&mousex, &mousey);
+        mousepos.x = (char)((mousex - M_WIDTH) / G_SIZE);
+        mousepos.y = (char)(mousey / G_SIZE);
+
         while(SDL_PollEvent(&e)){
             switch(e.type){
                 case (SDL_QUIT):
@@ -91,6 +144,46 @@ int main(int argc, char** args){
                 /*         size --; */
                 /*     break; */
                 /* } */
+                case (SDL_KEYDOWN):
+                    switch(e.key.keysym.scancode){
+                        case SDL_SCANCODE_I:
+                            mode = insert;
+                            break;
+                        case SDL_SCANCODE_D:
+                            mode = delete;
+                            break;
+                        case SDL_SCANCODE_W:
+                            mode = wiring;
+                            break;
+                        case SDL_SCANCODE_R:
+                            mode = running;
+                            break;
+                        case SDL_SCANCODE_ESCAPE:
+                            mode = none;
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case (SDL_MOUSEBUTTONDOWN):
+                    switch(mode){
+                        case insert:
+                            if (mousex < 210) break;
+                            InsertComponent(mousepos);
+                            break;
+                        case delete:
+                            if (mousex < 210) break;
+                            DeleteComponent(mousepos);
+                            break;
+                        case running:
+                            if (mousex < 210) break;
+                            break;
+                        case wiring:
+                            if (mousex < 210) break;
+                            break;
+                        default:
+                            break;
+                    }
                 default:
                     break;
             }
@@ -99,9 +192,11 @@ int main(int argc, char** args){
         SDL_SetRenderDrawColor(renderer, BG);
         SDL_RenderClear(renderer);
 
-        DrawGrid(size);
+        DrawGrid();
+        if (mousex > M_WIDTH)
+            Highlight(mousepos, mode);
 
-        SDL_Delay(1.0/60);
+        SDL_Delay(10);
         SDL_RenderPresent(renderer);
     }
     return 0;
