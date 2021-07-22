27b6f9fb8c89fdacdd30c47592de9a6ec38ca226
AshuBohara <ashubohara888@gmail.com>
2021-07-22 12:20:36 +0545

    modified build.bat for ttf dependencies

--- build.bat
+++ build.bat
@@ -22,13 +22,16 @@ popd
 
 :SkipDownloadSDL
 
-if exist "Libraries/SDL2MinGw" goto SkipDownloadSDLMinGw
+if exist "Libraries/SDL2MinGw/SDL2-2.0.14/" if exist "Libraries/SDL2MinGw/SDL2_ttf-2.0.15/" goto SkipDownloadSDLMinGw
 
 pushd Libraries
 mkdir SDL2MinGw
 curl "https://www.libsdl.org/release/SDL2-devel-2.0.14-mingw.tar.gz" --output SDL2MinGw.tar.gz
+curl "https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-devel-2.0.15-mingw.tar.gz" --output SDL2MinGw_ttf.tar.gz
 tar -xf SDL2MinGw.tar.gz -C SDL2MinGw
+tar -xf SDL2MinGw_ttf.tar.gz -C SDL2MinGw
 del SDL2MinGw.zip
+del SDL2MinGw_ttf.tar.gz
 popd
 
 :SkipDownloadSDLMinGw
@@ -41,6 +44,10 @@ set SDL2_DLL="..\..\Libraries\SDL2\SDL2-2.0.14\lib\x64\SDL2.dll"
 set SDL2MinGw_Include="../../Libraries/SDL2MinGw/SDL2-2.0.14/i686-w64-mingw32/include/"
 set SDL2MinGw_Library="../../Libraries/SDL2MinGw/SDL2-2.0.14/i686-w64-mingw32/lib/"
 set SDL2MinGw_DLL="..\..\Libraries\SDL2MinGw\SDL2-2.0.14\i686-w64-mingw32\bin\SDL2.dll"
+
+set SDL2MinGw_ttf_Library="../../Libraries/SDL2MinGw/SDL2_ttf-2.0.15/i686-w64-mingw32/lib/"
+set SDL2MinGw_ttf_Include = "../../Libraries/SDL2MinGw/SDL2_ttf-2.0.15/i686-w64-mingw32/include"
+set SDL2MinGw_ttf_DLL="..\..\Libraries\SDL2MinGw\SDL2_ttf-2.0.15\i686-w64-mingw32\bin\SDL2_ttf.dll"
 
 if not exist "bin" mkdir bin
 
@@ -65,7 +72,8 @@ IF %ERRORLEVEL% NEQ 0 goto SkipCLANG
 echo Building with CLANG
 pushd bin
 xcopy %SDL2_DLL% .\ /Y
-call clang -I%SDL2_Include% -L%SDL2_Library% %CLANGFlags% %SourceFiles% -o %OutputName% -lSDL2main -lSDL2 -lShell32 -Xlinker -subsystem:console
+xcopy %SDL2MinGw_ttf_DLL% .\ /Y
+call clang -I%SDL2_Include% -I%SDL2MinGw_ttf_Include% -L%SDL2_Library% -L%SDL2MinGw_ttf_Library% %CLANGFlags% %SourceFiles% -o %OutputName% -lSDL2main -lSDL2 -lSDL2-ttf -lShell32 -Xlinker -subsystem:console
 popd
 goto Finished
 
@@ -76,7 +84,8 @@ IF %ERRORLEVEL% NEQ 0 goto SkipGCC
 echo Building with GCC
 pushd bin
 xcopy %SDL2MinGw_DLL% .\ /Y
-call gcc -I%SDL2MinGw_Include% -L%SDL2MinGw_Library% %GCCFlags% %SourceFiles% -o %OutputName% -w -Wl,-subsystem,console -lmingw32 -lSDL2main -lSDL2
+xcopy %SDL2MinGw_ttf_DLL% .\ /Y
+call gcc -I%SDL2MinGw_Include% -I%SDL2MinGw_ttf_Include% -L%SDL2MinGw_Library% -L%SDL2MinGw_ttf_Library% %GCCFlags% %SourceFiles% -o %OutputName% -w -Wl,-subsystem,console -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf
 popd
 goto Finished
 
