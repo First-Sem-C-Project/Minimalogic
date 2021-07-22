9fe928d88ad82a384cf09ab5f1b02c1500aabdfb
ronejfourn <rs2102@protonmail.com>
2021-07-22 18:15:11 +0545

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,10 +1,8 @@
 @echo off
 
-REM copied from Cellular-automaton build file
-
 setlocal
 
-set SourceFiles=../main.c ../component.c
+set SourceFiles=../../main.c ../../component.c
 set OutputName=MinimaLogic.exe
 
 pushd ..
@@ -34,55 +32,66 @@ popd
 :SkipDownloadSDLMinGw
 popd
 
-set SDL2_Include="../../Libraries/SDL2/SDL2-2.0.14/"
-set SDL2_Library="../../Libraries/SDL2/SDL2-2.0.14/lib/x64/"
-set SDL2_DLL="..\..\Libraries\SDL2\SDL2-2.0.14\lib\x64\SDL2.dll"
+set SDL2_Include="../../../Libraries/SDL2/SDL2-2.0.14/"
+set SDL2_Library="../../../Libraries/SDL2/SDL2-2.0.14/lib/x64/"
+set SDL2_DLL="..\..\..\Libraries\SDL2\SDL2-2.0.14\lib\x64\SDL2.dll"
 
-set SDL2MinGw_Include="../../Libraries/SDL2MinGw/SDL2-2.0.14/i686-w64-mingw32/include/"
-set SDL2MinGw_Library="../../Libraries/SDL2MinGw/SDL2-2.0.14/i686-w64-mingw32/lib/"
-set SDL2MinGw_DLL="..\..\Libraries\SDL2MinGw\SDL2-2.0.14\i686-w64-mingw32\bin\SDL2.dll"
+set SDL2MinGw_Include="../../../Libraries/SDL2MinGw/SDL2-2.0.14/x86_64-w64-mingw32/include/"
+set SDL2MinGw_Library="../../../Libraries/SDL2MinGw/SDL2-2.0.14/x86_64-w64-mingw32/lib/"
+set SDL2MinGw_DLL="..\..\..\Libraries\SDL2MinGw\SDL2-2.0.14\x86_64-w64-mingw32\bin\SDL2.dll"
 
 if not exist "bin" mkdir bin
 
-echo -------------------------------------
-echo SDL2 inlude path:  %SDL2_Include%
-echo SDL2 library path: %SDL2_Library%
-echo SDL2MinGw inlude path:  %SDL2_Include%
-echo SDL2MinGw library path: %SDL2_Library%
-
+echo ----------------------------------------
+:MSVC
 where cl >nul 2>nul
-if %ERRORLEVEL% neq 0 goto SkipMSVC
+IF %ERRORLEVEL% NEQ 0 goto SkipMSVC
 echo Building with MSVC
-pushd bin
+if not exist "bin\MSVCBuild" mkdir bin\MSVCBuild
+pushd bin\MSVCBuild
 xcopy %SDL2_DLL% .\ /Y
 call cl -I%SDL2_Include% -nologo %CLFlags% -Zi -EHsc %SourceFiles% -Fe%OutputName% /link /LIBPATH:%SDL2_Library% SDL2.lib SDL2main.lib Shell32.lib /subsystem:console
-goto Finished
+popd
+echo MSVC Build Complete
+echo ----------------------------------------
+goto CLANG
 
 :SkipMSVC
+echo MSVC not found. Skipping build with MSVC. 
+echo ----------------------------------------
 
+:CLANG
 where clang >nul 2>nul
 IF %ERRORLEVEL% NEQ 0 goto SkipCLANG
 echo Building with CLANG
-pushd bin
+if not exist "bin\ClangBuild" mkdir bin\ClangBuild
+pushd bin\ClangBuild
 xcopy %SDL2_DLL% .\ /Y
 call clang -I%SDL2_Include% -L%SDL2_Library% %CLANGFlags% %SourceFiles% -o %OutputName% -lSDL2main -lSDL2 -lShell32 -Xlinker -subsystem:console
+echo Clang Build Complete
+echo ----------------------------------------
 popd
-goto Finished
+goto GCC
 
 :SkipCLANG
+echo Clang not found. Skipping build with Clang. 
+echo ----------------------------------------
 
+:GCC
 where gcc >nul 2>nul
 IF %ERRORLEVEL% NEQ 0 goto SkipGCC
 echo Building with GCC
-pushd bin
+if not exist "bin\GccBuild" mkdir bin\GccBuild
+pushd bin\GccBuild
 xcopy %SDL2MinGw_DLL% .\ /Y
 call gcc -I%SDL2MinGw_Include% -L%SDL2MinGw_Library% %GCCFlags% %SourceFiles% -o %OutputName% -w -Wl,-subsystem,console -lmingw32 -lSDL2main -lSDL2
+echo Gcc Build Complete
+echo ----------------------------------------
 popd
 goto Finished
 
 :SkipGCC
-
-echo No compiler found, please setup compiler. Instructions: https://github.com/IT-Club-Pulchowk/C-Programming-Guide
+echo Gcc not found. Skipping build with Gcc. 
+echo ----------------------------------------
 
 :Finished
-echo -------------------
