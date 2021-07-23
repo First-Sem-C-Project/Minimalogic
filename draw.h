43332e3b2ab74dc10e9eb0105107f16e39aa6a14
AshuBohara <ashubohara888@gmail.com>
2021-07-23 06:54:39 +0545

    fixed memory leak. removed some redundancies

--- draw.h
+++ draw.h
@@ -1,48 +1,49 @@
 SDL_Texture* FontTexture(SDL_Renderer *renderer, const char* message){
     TTF_Font* sans = TTF_OpenFont("fonts/sans.ttf", 50);
+    SDL_Surface* textSurface = NULL;
+    SDL_Texture* textTexture = NULL;
     if(sans == NULL){
         printf("Failed to load the font: %s\n", TTF_GetError());
     }
     else{
-        SDL_Surface* textSurface = NULL;
-        SDL_Texture* textTexture = NULL;
         SDL_Color white = {WHITE, 200};
         textSurface = TTF_RenderText_Solid(sans, message, white);
-
-        if(textSurface == NULL){
-            printf("Failed to load the surface\n");
-        }
-        else{
-            textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
-            if(textTexture == NULL){
-                printf("Failed to load the texture: %s\n",SDL_GetError());
-            }
-            SDL_FreeSurface(textSurface);
-        }
-        return textTexture;
+        textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
     }
+
+    SDL_FreeSurface(textSurface);
+    TTF_CloseFont(sans);
+    return textTexture;
 }
 
 void RenderGateText(SDL_Renderer *renderer, SDL_Rect compo, Type type){
     SDL_Rect textRect = {compo.x+compo.w/4, compo.y+compo.h/4, compo.w/2, compo.h/2};
+    SDL_Texture* text = NULL;
     switch (type){
         case(g_and):
-            SDL_RenderCopy(renderer, FontTexture(renderer, "AND"), NULL, &textRect);
+            text = FontTexture(renderer, "AND");
+            SDL_RenderCopy(renderer, text, NULL, &textRect);
             break;
         case(g_or):
-            SDL_RenderCopy(renderer, FontTexture(renderer, "OR"), NULL, &textRect);
+            text = FontTexture(renderer, "OR");
+            SDL_RenderCopy(renderer, text, NULL, &textRect);
             break;
         case(g_nand):
-            SDL_RenderCopy(renderer, FontTexture(renderer, "NAND"), NULL, &textRect);            
+            text = FontTexture(renderer, "NAND");
+            SDL_RenderCopy(renderer, text, NULL, &textRect);            
             break;
         case(g_nor):
-            SDL_RenderCopy(renderer, FontTexture(renderer, "NOR"), NULL, &textRect);           
+            text = FontTexture(renderer, "NOR");
+            SDL_RenderCopy(renderer, text, NULL, &textRect);           
             break;
         case(g_xor):
-            SDL_RenderCopy(renderer, FontTexture(renderer, "XOR"), NULL, &textRect);            
+            text = FontTexture(renderer, "XOR");
+            SDL_RenderCopy(renderer, text, NULL, &textRect);            
             break;
         default:
-            SDL_RenderCopy(renderer, FontTexture(renderer, "XNOR"), NULL, &textRect);            
+            text = FontTexture(renderer, "XNOR");
+            SDL_RenderCopy(renderer, text, NULL, &textRect);            
             break;
     }
+    SDL_DestroyTexture(text);
 }
