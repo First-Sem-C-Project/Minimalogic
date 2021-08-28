8d154cb0e087be15ca3b164ccb3d6dfe09415f65
ronejfourn <rs2102@protonmail.com>
2021-08-28 21:23:37 +0545

    build.bat downloads fonts and libs in .

--- build.bat
+++ build.bat
@@ -5,8 +5,6 @@ setlocal
 set SourceFiles=../../src/*.c
 set OutputName=MinimaLogic.exe
 
-pushd ..
-
 set CLFlags=-Od
 set CLANGFlags=-g -gcodeview
 set GCCFlags=-O
@@ -21,7 +19,30 @@ echo Optimize Build configured
 echo -------------------------------------
 :DoneConfig
 
-if not exist "Libraries" mkdir Libraries
+if not exist "fonts" mkdir fonts
+pushd fonts
+if exist "fonts/anka-coder-narrow/AnkaCoder-C75-r.ttf" goto SkipDownloadUIFont
+echo ----------------------------------------
+echo Downloading UI Font
+echo ----------------------------------------
+curl "https://fontlibrary.org/assets/downloads/anka-coder-narrow/4ed9dd9fcfa020d9f9c3de3fffb2b2ad/anka-coder-narrow.zip" --output anka.zip
+mkdir anka-coder-narrow
+tar -zxvf anka.zip -C anka-coder-narrow
+del anka.zip
+:SkipDownloadUIFont
+if exist "fonts/segment7/Segment7Standard.otf" goto SkipDownloadLEDFont
+echo ----------------------------------------
+echo Downloading LED Font
+echo ----------------------------------------
+curl "https://fontlibrary.org/assets/downloads/segment7/4cc82137fc130708919bf201c0dc9aae/segment7.zip" --output seg.zip
+mkdir segment7
+tar -zxvf seg.zip -C segment7
+del seg.zip
+:SkipDownloadLEDFont
+popd
+
+if not exist "libs" mkdir libs
+pushd libs
 where cl >nul 2>nul
 IF %ERRORLEVEL% NEQ 0 goto CheckForClang
 goto DownloadSDL
@@ -29,56 +50,60 @@ goto DownloadSDL
 where clang >nul 2>nul
 IF %ERRORLEVEL% NEQ 0 goto SkipDownloadSDL
 :DownloadSDL
-if exist "Libraries/SDL2/SDL2-2.0.14" if exist "Libraries/SDL2/SDL2_ttf-2.0.15" goto SkipDownloadSDL
-echo Downloading SDL
-
-pushd Libraries
+if exist "libs/SDL2/" goto SkipDownloadSDL
 mkdir SDL2
+echo ----------------------------------------
+echo Downloading SDL
+echo ----------------------------------------
 curl "https://www.libsdl.org/release/SDL2-devel-2.0.14-VC.zip" --output SDL2.zip
-curl "https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-devel-2.0.15-VC.zip" --output SDL2_ttf.zip
 tar -zxvf SDL2.zip -C SDL2
-tar -zxvf SDL2_ttf.zip -C SDL2
 del SDL2.zip
+echo ----------------------------------------
+echo Downloading SDL_ttf
+echo ----------------------------------------
+curl "https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-devel-2.0.15-VC.zip" --output SDL2_ttf.zip
+tar -zxvf SDL2_ttf.zip -C SDL2
 del SDL2_ttf.zip
 xcopy /Y SDL2\SDL2_ttf-2.0.15\include\SDL_ttf.h SDL2\SDL2-2.0.14\include
 ren "SDL2\SDL2-2.0.14\include" "SDL2"
-popd
 
 :SkipDownloadSDL
 
 where gcc >nul 2>nul
 IF %ERRORLEVEL% NEQ 0 goto SkipDownloadSDLMinGw
 
-if exist "Libraries/SDL2MinGw/SDL2-2.0.14/" if exist "Libraries/SDL2MinGw/SDL2_ttf-2.0.15/" goto SkipDownloadSDLMinGw
-echo Downloading SDLMinGw
-
-pushd Libraries
+if exist "libs/SDL2MinGw/" goto SkipDownloadSDLMinGw
 mkdir SDL2MinGw
+echo ----------------------------------------
+echo Downloading SDLMinGw
+echo ----------------------------------------
 curl "https://www.libsdl.org/release/SDL2-devel-2.0.14-mingw.tar.gz" --output SDL2MinGw.tar.gz
-curl "https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-devel-2.0.15-mingw.tar.gz" --output SDL2MinGw_ttf.tar.gz
 tar -xf SDL2MinGw.tar.gz -C SDL2MinGw
+del SDL2MinGw.tar.gz
+echo ----------------------------------------
+echo Downloading SDLMinGw_ttf
+echo ----------------------------------------
+curl "https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-devel-2.0.15-mingw.tar.gz" --output SDL2MinGw_ttf.tar.gz
 tar -xf SDL2MinGw_ttf.tar.gz -C SDL2MinGw
-del SDL2MinGw.zip
 del SDL2MinGw_ttf.tar.gz
 xcopy /Y SDL2MinGw\SDL2_ttf-2.0.15\x86_64-w64-mingw32\include\SDL2\SDL_ttf.h SDL2MinGw\SDL2-2.0.14\x86_64-w64-mingw32\include\SDL2
-popd
 
 :SkipDownloadSDLMinGw
 popd
 
-set SDL2_Include="../../../Libraries/SDL2/SDL2-2.0.14/"
-set SDL2_Library="../../../Libraries/SDL2/SDL2-2.0.14/lib/x64/"
-set SDL2_DLL="..\..\..\Libraries\SDL2\SDL2-2.0.14\lib\x64\SDL2.dll"
+set SDL2_Include="../../libs/SDL2/SDL2-2.0.14/"
+set SDL2_Library="../../libs/SDL2/SDL2-2.0.14/lib/x64/"
+set SDL2_DLL="..\..\libs\SDL2\SDL2-2.0.14\lib\x64\SDL2.dll"
 
-set SDL2_ttf_Library="../../../Libraries/SDL2/SDL2_ttf-2.0.15/lib/x64"
-set SDL2_ttf_DLL="..\..\..\Libraries\SDL2\SDL2_ttf-2.0.15\lib\x64\*.dll"
+set SDL2_ttf_Library="../../libs/SDL2/SDL2_ttf-2.0.15/lib/x64"
+set SDL2_ttf_DLL="..\..\libs\SDL2\SDL2_ttf-2.0.15\lib\x64\*.dll"
 
-set SDL2MinGw_Include="../../../Libraries/SDL2MinGw/SDL2-2.0.14/x86_64-w64-mingw32/include/"
-set SDL2MinGw_Library="../../../Libraries/SDL2MinGw/SDL2-2.0.14/x86_64-w64-mingw32/lib/"
-set SDL2MinGw_DLL="..\..\..\Libraries\SDL2MinGw\SDL2-2.0.14\x86_64-w64-mingw32\bin\SDL2.dll"
+set SDL2MinGw_Include="../../libs/SDL2MinGw/SDL2-2.0.14/x86_64-w64-mingw32/include/"
+set SDL2MinGw_Library="../../libs/SDL2MinGw/SDL2-2.0.14/x86_64-w64-mingw32/lib/"
+set SDL2MinGw_DLL="..\..\libs\SDL2MinGw\SDL2-2.0.14\x86_64-w64-mingw32\bin\SDL2.dll"
 
-set SDL2MinGw_ttf_Library="../../../Libraries/SDL2MinGw/SDL2_ttf-2.0.15/x86_64-w64-mingw32/lib/"
-set SDL2MinGw_ttf_DLL="..\..\..\Libraries\SDL2MinGw\SDL2_ttf-2.0.15\x86_64-w64-mingw32\bin\*.dll"
+set SDL2MinGw_ttf_Library="../../libs/SDL2MinGw/SDL2_ttf-2.0.15/x86_64-w64-mingw32/lib/"
+set SDL2MinGw_ttf_DLL="..\..\libs\SDL2MinGw\SDL2_ttf-2.0.15\x86_64-w64-mingw32\bin\*.dll"
 set UIFont="..\..\fonts\anka-coder-narrow\*.ttf"
 set LEDFont="..\..\fonts\segment7\*.otf"
 
